7597cd13b013447949c033002e23c87917b34a22
mwanjajoel <joelsilverworks@gmail.com>
2020-02-18 18:42:24 +0300

    added more requirements

---
+++ setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import setup
+
+# the directory containing this file 
+HERE = pathlib.Path(__file__).parent
+
+# The Readme file
+README = (HERE / "README.md").read_text()
+
+# This does all the work once called
+setup(
+    name="django-momoapi",
+    version="1.0.0",
+    description="This is a Django package for the MTN MoMo API."
+    long_description=README,
+    url="https://github.com/mwanjajoel/django-momoapi",
+    author="mwanjajoel",
+    author_email="joelsilverworks@gmail.com",
+    license="MIT",
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ]
+   
+)
