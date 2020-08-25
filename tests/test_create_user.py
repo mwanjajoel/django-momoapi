a4fa081b085b75f1b4e38093a47d6ad1672e6b75
mwanjajoel <joelsilverworks@gmail.com>
2020-08-25 23:17:15 +0300

    added the first test

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 
 # create a Pytest mark to test user creation so as to access the db 
 @pytest.mark.django_db
-def create_user_test():
-    User.objects.create_user('momoapi', 'momoapi@mwanjajoel.com', 'momopassword')
+def test_user_create():
+    User.objects.create_user(
+        username='momoapi', 
+        email='momoapi@mwanjajoel.com', 
+        password='momopassword')
     assert User.objects.count() == 1
