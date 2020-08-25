56538fd8deafaa12afc103d46ad266b376afc77f
mwanjajoel <joelsilverworks@gmail.com>
2020-08-25 23:28:09 +0300

    removed pytest marker and unused import

--- tests/unit/test_create_user.py
+++ tests/unit/test_create_user.py
@@ -1,7 +1,5 @@
 import pytest
-from django.contrib.auth.models import User 
 
-# create a Pytest mark to test user creation so as to access the db 
-@pytest.mark.django_db
+# create a simple Pytest 
 def test_user_create():
     pass
